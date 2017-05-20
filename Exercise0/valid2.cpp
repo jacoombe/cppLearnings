5af9acaf3024a68e229c39afd475d53ec07c7fd5
James Coombes <jamespjcoombes@gmail.com>
2017-05-20 16:35:23 +0100

    Tiding up my chages for exercise 0-6

--- Exercise0/valid2.cpp
+++ Exercise0/valid2.cpp
@@ -1,6 +1,6 @@
 // a small c++ program
 #include <iostream>
 
-// This is invalid because of the lack of {}
+// This is valid because curly brackets can be nested alot
 
 int main () {{{{{{std::cout << "Hello Wonderful World!" << std::endl;}}}}}}
